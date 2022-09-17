bec19cec8c9cbdbf4512eb49bc6fe7091fce45cb
Errol Stewart <Errol.F.Stewart@gmail.com>
2022-09-17 12:10:33 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import react from "react"
+import react-dom from "react-dom"
+
+ReactDOM.render("
+<div>
+<h1>This is the Head</h1>
+<p>This is a Body</p>
+<ul>
+<ul>
+<ul>"), document.getElementByID
+</div>
